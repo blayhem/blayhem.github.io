b3282d69b58d4b4c139e3a4418e0e5e8703dd21d
Daniel Fernandez Rico <deferrico@gmail.com>
2016-02-01 17:02:06 +0100

    Landing test.

---
+++ landing_test/Scripts/script.js
@@ -0,0 +1,52 @@
+jQuery(document).ready(function($) {
+	var text = [ 
+		
+		"Hello, friend,", 
+		"My name is Daniel Fernandez",
+		"I'm a junior developer and designer",
+		"as well as a CS student.",
+        "Welcome to my page!"
+
+	];
+
+var htmlElement = document.getElementById("text");
+var codioLink = document.getElementById("codioLink");    
+
+var realisticTypewriter = new RealisticTypewriter();
+
+// 10% typo rate
+realisticTypewriter.accuracy = 94; 
+
+// typing speed will be from 5 to 10 characters per second.
+realisticTypewriter.minimumCharactersPerSecond = 5;
+realisticTypewriter.maximumCharactersPerSecond = 10;
+
+// waits at least 0.5 second and at most 1 second before it starts typing
+realisticTypewriter.minimumInitialDelay = 500;
+realisticTypewriter.maximumInitialDelay = 1200;
+    
+var letras = jQuery('.letra');
+
+function writeText (counter) {
+
+	realisticTypewriter.type(text[counter],htmlElement, function () {
+		
+		if (counter < (text.length - 1)) {
+			
+			setTimeout(function() {
+				counter++;
+				document.getElementById('text').innerHTML = '';
+				writeText(counter);
+                
+			}, 1000); } 
+      
+		else {
+            $('#portfolio, .email').delay(1500).fadeIn(500);
+
+            
+		}
+	});
+}
+
+writeText(0);
+});
